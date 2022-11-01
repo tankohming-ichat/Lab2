72760b69da890f28b43887f5e28f72981a782e4a
KohMing <ming.21@ichat.sp.edu.sg>
2022-11-01 10:07:37 +0800

    v1.1

--- Lab2.py
+++ Lab2.py
@@ -1,15 +1,60 @@
-def calculate_bmi(height, weight):
-    print("Height = " + str(height))
-    print("Weight = " + str(weight))
-    #Add code here to calculate BMI
-    bmi = weight/(height*height)
-    #Add code here to display calculate BMI
-    if (bmi < 18.5):
-        string="Under Weight"
-    elif (bmi < 25.0):
-        string="Normal Weight"
-    else:
-        string="Over Weight"
-    print("BMI is %s, which is %s" %(bmi,string))
 
-calculate_bmi(weight=57, height=1.73)
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5,67,32)")
+
+def get_user_input():
+    listholder = []
+
+    #n = int(input("How many values are you entering?"))
+
+    #for z in range(0, n):
+    x = str(input())
+    #listholder.append(x)
+    list = x.split(",")
+
+    for i in range(0, len(list)):
+        list[i] = float(list[i])
+
+    return list
+
+def calc_average(temp_list):
+    tempsum = 0
+    for i in range(0, len(temp_list)):
+        tempsum += temp_list[i]
+    avg_temp = tempsum/len(temp_list)
+    return avg_temp
+
+def calc_min_max(temp_list):
+    min=temp_list[0]
+    max=temp_list[0]
+    for i in range(0,len(temp_list)):
+        if(min>temp_list[i]):
+            min = temp_list[i]
+
+        if (max < temp_list[i]):
+            max = temp_list[i]
+
+    listholder = [min,max]
+    return listholder
+
+
+def sort_temperature(temp_list):
+    temp_list.sort()
+    return temp_list
+
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    temp_list = get_user_input()
+    avgTemp = calc_average(temp_list)
+    minmax_list = calc_min_max(temp_list)
+    sorted_list = sort_temperature(temp_list)
+    print("The average temperature of the values that you entered is: ",avgTemp)
+    print("The lowest temperature you entered is: ",minmax_list[0],end='\n')
+    print("The highest temperature you entered is: ",minmax_list[1])
+
+
+if __name__ == "__main__":
+    main()
+
